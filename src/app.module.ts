3158f6dac0654de448a72bb116e1789fd78902de
Hazeem Salim <hazeem.salim@iflix.com>
2020-06-13 22:10:01 +0800

    Remove app service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { PaymentsController } from './controllers/payments.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
   controllers: [PaymentsController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
